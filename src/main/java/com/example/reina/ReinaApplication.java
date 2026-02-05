efa6fdb2ed0f46e6e7c6c1f3d1fa75e1fbed8040
reina27csb-ui <reina.27csb@licet.ac.in>
2026-02-05 11:16:17 +0530

    first

---
+++ src/main/java/com/example/reina/ReinaApplication.java
@@ -0,0 +1,13 @@
+package com.example.reina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reina/ReinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
