ff284c696cc35b0cca8c1286d37095d7de3f08d8
RAJARAJESWARI-K <rajarajeswari.27csb@licet.ac.in>
2026-02-17 14:59:00 +0530

    update

---
+++ team_08/src/main/java/com/example/team_08/Team08Application.java
@@ -0,0 +1,13 @@
+package com.example.team_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team08Application.class, args);
+	}
+
+}
---
+++ team_08/src/test/java/com/example/team_08/Team08ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team08ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
